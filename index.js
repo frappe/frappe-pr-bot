fbadefd061857b523a9f187fe51b25f32bb5ece3
Ameya Shenoy <shenoy.ameya@gmail.com>
2018-10-04 15:52:19 +0530

    fix(lint): minor linting fixes
    Signed-off-by: Ameya Shenoy <shenoy.ameya@gmail.com>

--- index.js
+++ index.js
@@ -5,12 +5,12 @@
 module.exports = app => {
 
   // Logging the start
-  app.log('Alright people, we\'re functional!, Let\' do this')
+  app.log('Alright people, we\'re functional!, Let\' do this');
 
   // Triggered when a Pull Request is Opened or Reopened
   app.on(['pull_request.opened', 'pull_request.reopened'], async context => {
-    verify_pr_branch(context)
-  })
+    verify_pr_branch(context);
+  });
 
   /**
    * This function verifies the branch on which the pull request was sent,
@@ -21,35 +21,34 @@ module.exports = app => {
    * @param {Object} context Object sent in by the github webhook
    */
   let verify_pr_branch = (context) => {
-    invalid_pr_branches = ['master', 'staging']
     const pr_on_branch  = context.payload.pull_request.base.ref;
 
-    if ( invalid_pr_branches.includes(pr_on_branch) ){
+    const incorrect_to_correct_branch_map = {
+      staging : 'staging-fixes',
+      master  : 'hotfix'
+    };
 
-      const correct_branch = {
-        staging : 'staging-fixes',
-        master  : 'hotfix'
-      }
+    if ( pr_on_branch in incorrect_to_correct_branch_map ){
 
       const pr_creator        = context.payload.pull_request.user.login;
-      const correct_pr_branch = correct_branch[pr_on_branch];
+      const correct_pr_branch = incorrect_to_correct_branch_map[pr_on_branch];
 
-      const org_name       = context.payload.pull_request.base.user.login
-      const repo_name      = context.payload.pull_request.base.repo.name
-      const pr_from_repo   = context.payload.pull_request.head.user.login
-      const pr_from_branch = context.payload.pull_request.head.ref
+      const org_name       = context.payload.pull_request.base.user.login;
+      const repo_name      = context.payload.pull_request.base.repo.name;
+      const pr_from_repo   = context.payload.pull_request.head.user.login;
+      const pr_from_branch = context.payload.pull_request.head.ref;
 
       const new_pr_url        = `https://github.com/${org_name}/${repo_name}/compare/`+
-      `${correct_pr_branch}...${pr_from_repo}:${pr_from_branch}?expand=1`
+      `${correct_pr_branch}...${pr_from_repo}:${pr_from_branch}?expand=1`;
 
       const pull_request_comment = context.issue({ body: `Hey @${pr_creator} Thanks ` + 
         `for opening a Pull Request! \nHowever we do not accept PRs on the ` + 
         `\`${pr_on_branch}\` branch. Please open the PR on the \`${correct_pr_branch}\` ` + 
         `branch. \n\n You may click [here](${new_pr_url}) to do the same! \n` +
-        `cc: @frappe/release-team ` })
+        `cc: @frappe/release-team ` });
 
-      context.github.issues.createComment(pull_request_comment)
-      context.github.issues.edit(context.issue({state: "closed"}))
+      context.github.issues.createComment(pull_request_comment);
+      context.github.issues.edit(context.issue({state: "closed"}));
 
       return
     }
