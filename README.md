b35daeb760db40ba7ee59b35d9c0b9b99a4db33e
Ameya Shenoy <shenoy.ameya@gmail.com>
2018-10-04 13:57:37 +0530

    Initial Commit
    - app in functional state
    - closes PRs to staging and hotfix
    
    Signed-off-by: Ameya Shenoy <shenoy.ameya@gmail.com>

---
+++ README.md
@@ -0,0 +1,23 @@
+# frappe-pr-bot
+
+> A GitHub App built with [Probot](https://probot.github.io) that Automation of minor PR tasks
+
+## Setup
+
+```sh
+# Install dependencies
+npm install
+
+# Run the bot
+npm start
+```
+
+## Contributing
+
+If you have suggestions for how frappe-pr-bot could be improved, or want to report a bug, open an issue! We'd love all and any contributions.
+
+For more, check out the [Contributing Guide](CONTRIBUTING.md).
+
+## License
+
+[ISC](LICENSE) © 2018 Frappe Technologies <developers@erpnext.com> (https://frappe.io)
---
+++ index.js
@@ -0,0 +1,58 @@
+
+// This is the entry point for your Probot App.
+// @param {import('probot').Application} app - Probot's Application class.
+
+module.exports = app => {
+
+  // Logging the start
+  app.log('Alright people, we\'re functional!, Let\' do this')
+
+  // Triggered when a Pull Request is Opened or Reopened
+  app.on(['pull_request.opened', 'pull_request.reopened'], async context => {
+    verify_pr_branch(context)
+  })
+
+  /**
+   * This function verifies the branch on which the pull request was sent,
+   * and if it is one of staging or master, immediately closes it to prevevnt
+   * incorrect merges. It comments the correct branch on which the PR is 
+   * supposed to be opened with a link to creating one as well!
+   *
+   * @param {Object} context Object sent in by the github webhook
+   */
+  let verify_pr_branch = (context) => {
+    invalid_pr_branches = ['master', 'staging', 'develop']
+    const pr_on_branch  = context.payload.pull_request.base.ref;
+
+    if ( invalid_pr_branches.includes(pr_on_branch) ){
+
+      const correct_branch = {
+        staging : 'staging-fixes',
+        master  : 'hotfix'
+      }
+
+      const pr_creator        = context.payload.pull_request.user.login;
+      const correct_pr_branch = correct_branch[pr_on_branch];
+
+      const org_name       = context.payload.pull_request.base.user.login
+      const repo_name      = context.payload.pull_request.base.repo.name
+      const pr_from_repo   = context.payload.pull_request.head.user.login
+      const pr_from_branch = context.payload.pull_request.head.ref
+
+      const new_pr_url        = `https://github.com/${org_name}/${repo_name}/compare/`+
+      `${correct_pr_branch}...${pr_from_repo}:${pr_from_branch}?expand=1`
+
+      const pull_request_comment = context.issue({ body: `Hey @${pr_creator} Thanks ` + 
+        `for opening a Pull Request! \nHowever we do not accept PRs on the ` + 
+        `\`${pr_on_branch}\` branch. Please open the PR on the \`${correct_pr_branch}\` ` + 
+        `branch. \n\n You may click [here](${new_pr_url}) to do the same! \n` +
+        `cc: @frappe/release-team ` })
+
+      context.github.issues.createComment(pull_request_comment)
+      context.github.issues.edit(context.issue({state: "closed"}))
+
+      return
+    }
+  }
+}
+
