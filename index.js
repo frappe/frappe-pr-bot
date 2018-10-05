1b7a381b6aafe32534ef477e9120013a0ded53e9
Ameya Shenoy <shenoy.ameya@gmail.com>
2018-10-05 15:44:22 +0530

    style(lint): formatted all JS code
    Signed-off-by: Ameya Shenoy <shenoy.ameya@gmail.com>

--- index.js
+++ index.js
@@ -3,56 +3,51 @@
 // @param {import('probot').Application} app - Probot's Application class.
 
 module.exports = app => {
-
   // Logging the start
   app.log('Alright people, we\'re functional!, Let\' do this')
 
   // Triggered when a Pull Request is Opened or Reopened
   app.on(['pull_request.opened', 'pull_request.reopened'], async context => {
-    verify_pr_branch(context)
+    verifyPullRequestBranch(context)
   })
 
   /**
    * This function verifies the branch on which the pull request was sent,
    * and if it is one of staging or master, immediately closes it to prevevnt
-   * incorrect merges. It comments the correct branch on which the PR is 
+   * incorrect merges. It comments the correct branch on which the PR is
    * supposed to be opened with a link to creating one as well!
    *
    * @param {Object} context Object sent in by the github webhook
    */
-  let verify_pr_branch = (context) => {
-    invalid_pr_branches = ['master', 'staging']
-    const pr_on_branch  = context.payload.pull_request.base.ref;
+  let verifyPullRequestBranch = (context) => {
+    let invalidBranchesForPullRequest = ['master', 'staging']
+    const baseBranch = context.payload.pull_request.base.ref
 
-    if ( invalid_pr_branches.includes(pr_on_branch) ){
-
-      const correct_branch = {
-        staging : 'staging-fixes',
-        master  : 'hotfix'
+    if (invalidBranchesForPullRequest.includes(baseBranch)) {
+      const invalidToValidBranchMapping = {
+        staging: 'staging-fixes',
+        master: 'hotfix'
       }
 
-      const pr_creator        = context.payload.pull_request.user.login;
-      const correct_pr_branch = correct_branch[pr_on_branch];
+      const pullRequestCreator = context.payload.pull_request.user.login
+      const validBranch = invalidToValidBranchMapping[baseBranch]
 
-      const org_name       = context.payload.pull_request.base.user.login
-      const repo_name      = context.payload.pull_request.base.repo.name
-      const pr_from_repo   = context.payload.pull_request.head.user.login
-      const pr_from_branch = context.payload.pull_request.head.ref
+      const baseOrgName = context.payload.pull_request.base.user.login
+      const baseRepoName = context.payload.pull_request.base.repo.name
+      const headOrgName = context.payload.pull_request.head.user.login
+      const headRepoName = context.payload.pull_request.head.ref
 
-      const new_pr_url        = `https://github.com/${org_name}/${repo_name}/compare/`+
-      `${correct_pr_branch}...${pr_from_repo}:${pr_from_branch}?expand=1`
+      const validPullRequestURL = `https://github.com/${baseOrgName}/${baseRepoName}/compare/` +
+      `${validBranch}...${headOrgName}:${headRepoName}?expand=1`
 
-      const pull_request_comment = context.issue({ body: `Hey @${pr_creator} Thanks ` + 
-        `for opening a Pull Request! \nHowever we do not accept PRs on the ` + 
-        `\`${pr_on_branch}\` branch. Please open the PR on the \`${correct_pr_branch}\` ` + 
-        `branch. \n\n You may click [here](${new_pr_url}) to do the same! \n` +
+      const pullRequestComent = context.issue({ body: `Hey @${pullRequestCreator} Thanks ` +
+        `for opening a Pull Request! \nHowever we do not accept PRs on the ` +
+        `\`${baseBranch}\` branch. Please open the PR on the \`${validBranch}\` ` +
+        `branch. \n\n You may click [here](${validPullRequestURL}) to do the same! \n` +
         `cc: @frappe/release-team ` })
 
-      context.github.issues.createComment(pull_request_comment)
-      context.github.issues.edit(context.issue({state: "closed"}))
-
-      return
+      context.github.issues.createComment(pullRequestComent)
+      context.github.issues.edit(context.issue({ state: 'closed' }))
     }
   }
 }
-
