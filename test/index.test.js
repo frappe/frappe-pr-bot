d879e6247e947313837b6870127b1f55f9de7975
Ameya Shenoy <shenoy.ameya@gmail.com>
2018-10-05 12:39:25 +0530

    fix(tests): worte test cases for closing PRs
    Signed-off-by: Ameya Shenoy <shenoy.ameya@gmail.com>

--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,11 @@
+
 const { Application } = require('probot')
 // Requiring our app implementation
 const myProbotApp = require('..')
 
-const issuesOpenedPayload = require('./fixtures/issues.opened.json')
+const PRtoDevelopPayload = require('./fixtures/pull_request_to_develop.json')
+const PRtoMasterPayload  = require('./fixtures/pull_request_to_master.json')
+const PRtoStagingPayload = require('./fixtures/pull_request_to_staging.json')
 
 test('that we can run tests', () => {
   // your real tests go here
@@ -19,34 +22,46 @@ describe('My Probot app', () => {
     // This is an easy way to mock out the GitHub API
     github = {
       issues: {
-        createComment: jest.fn().mockReturnValue(Promise.resolve({}))
+        createComment: jest.fn().mockReturnValue(Promise.resolve({})),
+        edit         : jest.fn().mockReturnValue(Promise.resolve({}))
       }
     }
     // Passes the mocked out GitHub API into out app instance
     app.auth = () => Promise.resolve(github)
   })
 
-  test('creates a comment when an issue is opened', async () => {
-    // Simulates delivery of an issues.opened webhook
+  test('does nothing when PR opened on develop', async () => {
+    // Simulates delivery of an pull_request.opened webhook
     await app.receive({
-      name: 'issues.opened',
-      payload: issuesOpenedPayload
+      name: 'pull_request.opened',
+      payload: PRtoDevelopPayload
+    })
+  })
+
+  test('creates a comment and closes PR when its opened on master', async () => {
+    // Simulates delivery of an pull_request.opened webhook
+    await app.receive({
+      name: 'pull_request.opened',
+      payload: PRtoMasterPayload
     })
 
     // This test passes if the code in your index.js file calls `context.github.issues.createComment`
     expect(github.issues.createComment).toHaveBeenCalled()
+    expect(github.issues.edit).toHaveBeenCalled()
+  })
+
+  test('creates a comment and closes PR when its opened on staging', async () => {
+    // Simulates delivery of an pull_request.opened webhook
+    await app.receive({
+      name: 'pull_request.opened',
+      payload: PRtoStagingPayload
+    })
+
+    // This test passes if the code in your index.js file calls `context.github.issues.createComment`
+    expect(github.issues.createComment).toHaveBeenCalled()
+    expect(github.issues.edit).toHaveBeenCalled()
   })
 })
 
 // For more information about testing with Jest see:
 // https://facebook.github.io/jest/
-
-
-
-// ToDo
-
-// - [ ] Send PR to staging
-// - [ ] Send PR to develop
-// - [ ] Send PR to master
-// - [ ] Send PR to hotfix
-// - [ ] Send PR to staging-fixes
